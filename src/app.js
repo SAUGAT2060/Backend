3e368089676a9032a3726e3beb7fceca65794ca3
Saugat Rana <ranasaugat9@gmail.com>
2026-04-21 20:13:12 -0400

    Setup Configured

--- src/app.js
+++ src/app.js
@@ -12,4 +12,19 @@ app.use(express.json({limit:"20kb"}))
 app.use(express.urlencoded({extended:true , limit:"16 kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+// Import routes
+ import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+
+//https://localhost:8000//api/v1/users/register
+
+
+
+
 export {app}
